06fc38361cec98c82e9f7db46e3a261f7a272f1b
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-10-13 15:58:20 -0300

    Implementaçao da pagina de consulta aos orfanatos cadastrados e implementaçao do mapa do openstreet e mapbox.

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import mapMarkerImage from '../images/Local.svg';
+
+import 'leaflet/dist/leaflet.css'
+import '../styles/pages/orphanages-map.css';
 
 const OrphanagesMap = () => (
-    <h1>OrphanagesMap</h1>
+    <div id="page-map">
+        <aside>
+            <header>
+                <img src={mapMarkerImage} alt="Happy"/>
+
+                <h2>Escolha um orfanato no mapa</h2>
+                <p>Muitas crianças estão esperando a sua visita :)</p>
+            </header>
+
+            <footer>
+                <strong>Arapiraca</strong>
+                <p>Alagoas</p>
+            </footer>
+        </aside>
+
+        <Map center={[-9.738086, -36.6810374]} zoom={15} style={{width: '100%', height: '100%'}} >
+            {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+            <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+        </Map>
+
+        <Link to="" className="create-orphanage">
+            <FiPlus size="32" color="#FFF" />
+        </Link>
+    </div>
 )
 
 export default OrphanagesMap;
